c5a941e59b2f69479bd17d7257485edbcd61edfa
leonbahley <leon.bahley@icloud.com>
2022-11-03 15:46:48 +0100

    add-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
 import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import data from './statistics/data.json';
 import user from './profile/user.json';
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics stats={data} title="Upload stats" />
+    </div>
   );
 };
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import StatisticsItem from './StatisticsItem';
+import css from './statistics.module.css';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(item => (
+          <StatisticsItem
+            key={item.id}
+            label={item.label}
+            percentage={item.percentage}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
+Statistics.propTypes = {
+  title: PropTypes.string,
+};
