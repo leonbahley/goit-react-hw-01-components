5518a0e0e4f735a661b2622d672a22eefe62ab5e
leonbahley <leon.bahley@icloud.com>
2022-11-03 15:43:45 +0100

    add-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import Profile from './profile/Profile';
+import user from './profile/user.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
   );
 };
